8e53a95d45e35fe2c5df6dcddf31b2d068202172
nirhu <uhrinmartin@gmail.com>
2024-07-27 18:57:27 +0200

    feat(web-worker): 40-web-worker

--- apps/performance/40-web-workers/src/app/app.component.ts
+++ apps/performance/40-web-workers/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { PercentPipe } from '@angular/common';
+import { Component, computed, inject, signal } from '@angular/core';
 import { HeavyCalculationService } from './heavy-calculation.service';
 import { UnknownPersonComponent } from './unknown-person/unknown-person.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, UnknownPersonComponent],
+  imports: [PercentPipe, UnknownPersonComponent],
   providers: [HeavyCalculationService],
   selector: 'app-root',
   template: `
@@ -15,7 +15,9 @@ import { UnknownPersonComponent } from './unknown-person/unknown-person.componen
       (click)="discover()">
       Discover
     </button>
-    <div class="p-1 text-white">Progress: {{ loadingPercentage() }}%</div>
+    <div class="p-1 text-white">
+      Progress: {{ loadingPercentage() / 100 | percent: '1.1-1' }}
+    </div>
   `,
   host: {
     class: `flex flex-col h-screen w-screen bg-[#1f75c0]`,
@@ -23,10 +25,28 @@ import { UnknownPersonComponent } from './unknown-person/unknown-person.componen
 })
 export class AppComponent {
   private heavyCalculationService = inject(HeavyCalculationService);
+  private readonly workerLoadingPercentage = signal(0);
 
-  readonly loadingPercentage = this.heavyCalculationService.loadingPercentage;
+  readonly loadingPercentage = computed(
+    () =>
+      this.workerLoadingPercentage() ||
+      this.heavyCalculationService.loadingPercentage(),
+  );
 
   discover() {
-    this.heavyCalculationService.startLoading();
+    if (typeof Worker !== 'undefined') {
+      // Create a new
+      const worker = new Worker(
+        new URL('./heavy-calculation.worker', import.meta.url),
+      );
+      worker.onmessage = ({ data }) => {
+        this.workerLoadingPercentage.set(data);
+      };
+      worker.postMessage('hello');
+    } else {
+      // Web workers are not supported in this environment.
+      // You should add a fallback so that your program still executes correctly.
+      this.heavyCalculationService.startLoading();
+    }
   }
 }
